cfa65457845d4449feaf50a97270394575876f90
Benny Lee <lee.hsinchih@gmail.com>
2020-07-15 08:51:46 -0700

    finished seeding

--- postgres/datagen.js
+++ postgres/datagen.js
@@ -2,14 +2,19 @@ const fs = require('fs');
 const csvWriter = require('csv-write-stream')
 const faker = require('faker')
 
-const totalcount = 10000000;
+const totalPlaceCount = 1000000;
+const totalUserCount = 1000000;
+const totalBookingCount = 10000000;
 const writer1 = csvWriter();
 const writer2 = csvWriter();
 const writer3 = csvWriter();
 
 const placeGen = () =>{
   writer1.pipe(fs.createWriteStream(`./placeData.csv`));
-  for(let i=0;i<totalcount;i++){
+  for(let i=0;i<totalPlaceCount;i++){
+    if((i % (totalPlaceCount / 10)) === 0){
+      console.log("We are at placeGen "+i/totalPlaceCount);
+    }
     writer1.write({
       placeid: i+1,
       nightly_fee: Math.ceil(Math.random() * 300) + 60,
@@ -21,13 +26,16 @@ const placeGen = () =>{
       max_capacity: Math.ceil(Math.random() * 9) + 1
     })
   }
-  console.log("placegen done");
+  console.log("place Gen completed");
   writer1.end();
 }
 
 const userGen = () =>{
   writer2.pipe(fs.createWriteStream(`./userData.csv`));
-  for(let i=0;i<totalcount;i++){
+  for(let i=0;i<totalUserCount;i++){
+    if((i % (totalUserCount / 10)) === 0){
+      console.log("We are at userGen "+i/totalUserCount);
+    }
     writer2.write({
       userId: i+1,
       fullName: faker.name.firstName() + " "+ faker.name.lastName()
@@ -47,15 +55,20 @@ function randomTimeSlot(start) {
   dates.checkout = new Date(start.getTime()+Math.ceil((Math.random()*14)+1)*(24*60*60*1000)).toISOString();
    return dates;
 }
+writer3.pipe(fs.createWriteStream(`./bookingData4.csv`));
+let bookingCounter = 0;
 
 const bookingGen = () =>{
-  writer3.pipe(fs.createWriteStream(`./bookingData.csv`));
-  for(let i=0;i<totalcount;i++){
+  let writer3pass = true;
+  do{
+    if((bookingCounter % (totalBookingCount / 10)) === 0){
+      console.log("We are at bookingGen "+bookingCounter/totalBookingCount);
+    }
     let dates = randomTimeSlot(randomDate(new Date(2020, 8, 1), new Date(2021, 8, 1)))
-    writer3.write({
-      bookingId: i+1,
-      userId: Math.ceil(Math.random()*(totalcount-1))+1,
-      placeId: Math.ceil(Math.random()*(totalcount-1))+1,
+    let writer3obj = {
+      bookingId: (bookingCounter+1)+(10000000*4),
+      userId: Math.ceil(Math.random()*(totalUserCount-1))+1,
+      placeId: Math.ceil(Math.random()*(totalPlaceCount-1))+1,
       checkin: dates.checkin,
       checkout: dates.checkout,
       adults: Math.floor(Math.random() * 6) + 1,
@@ -63,17 +76,68 @@ const bookingGen = () =>{
       infants: Math.floor(Math.random() * 2) + 0,
       nightly_fee: Math.floor(Math.random() * 300) + 60,
       cleaning_fee: Math.floor(Math.random() * 150) + 50,
-      occupance_tax_rate: ((Math.floor(Math.random() * 15) + 8) / 100).toFixed(2),
-    })
+      occupance_tax_rate: ((Math.floor(Math.random() * 15) + 8) / 100).toFixed(2)
+    };
+    if(bookingCounter === totalBookingCount){
+      writer3.write(writer3obj);
+    }else{
+      writer3pass = writer3.write(writer3obj);
+    }
+    bookingCounter++;
+  } while(bookingCounter<totalBookingCount && writer3pass);
+
+  if(bookingCounter<totalBookingCount){
+      writer3.once("drain",bookingGen);
   }
-  console.log("booking gen completed");
-  writer3.end();
+  if(bookingCounter === totalBookingCount){
+    console.log("booking gen completed");
+    writer3.end();
+  }
 }
 
 const dataGen = () =>{
-  placeGen();
-  userGen();
+  // placeGen();
+  // userGen();
   bookingGen();
 }
+dataGen();
 
 module.exports = dataGen;
+
+
+// do{
+//   if((bookingGenCounter % (totalBookingCount / 10)) === 0){
+//    console.log("We are at bookingGen "+bookingGenCounter/totalBookingCount);
+//  }
+
+//  let writer2obj = {
+//    placeId: currentPlace,
+//    userId: currentUser,
+//    adults: adults,
+//    bookingId: bookingId,
+//    checkin: checkin,
+//    checkout: checout,
+//    children: children,
+//    cleaning_fee: cleaning_fee,
+//    infants: infants,
+//    nightly_fee: nightly_fee,
+//    occupance_tax_rate: occupance_tax_rate
+//  }
+
+//  if( bookingGenCounter === totalBookingCount){
+//    writer2.write(writer2obj);
+//  } else {
+//    writer2pass = writer2.write(writer2obj);
+//  }
+//  bookingGenCounter++;
+// } while(bookingGenCounter<=totalBookingCount && writer2pass);
+// if(bookingGenCounter<totalBookingCount){
+//  if(!writer2pass){
+//    writer2.once("drain",bookingGen);
+//  }
+// }
+// if(bookingGenCounter === totalBookingCount){
+//  console.log("booking gen completed");
+//  writer2.end();
+// }
+// }
--- postgres/seed.js
+++ postgres/seed.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const dataGen = require('./datagen.js');
 const { Client } = require('pg');
 
 // dataGen();
@@ -14,40 +13,14 @@ const client = new Client ({
 
 client.connect()
   .then( ()=> console.log('connected'))
-  .then( ()=> client.query("COPY place from '/Users/bennylee/Documents/galvanize/Calendar/postgres/placeData.csv' DELIMITER ',' CSV HEADER;"))
+  .then( ()=> client.query("COPY booking FROM '/Users/bennylee/Documents/galvanize/Calendar/postgres/bookingData2.csv' DELIMITER ',' CSV HEADER;"))
   .then( (res) => console.log(res))
-  .then( ()=> client.query("COPY user_by_id from '/Users/bennylee/Documents/galvanize/Calendar/postgres/userData.csv' DELIMITER ',' CSV HEADER;"))
+  .then( ()=> client.query("COPY booking FROM '/Users/bennylee/Documents/galvanize/Calendar/postgres/bookingData3.csv' DELIMITER ',' CSV HEADER;"))
   .then( (res) => console.log(res))
-  .then( ()=> client.query("COPY booking from '/Users/bennylee/Documents/galvanize/Calendar/postgres/bookingData.csv' DELIMITER ',' CSV HEADER;"))
+  .then( ()=> client.query("COPY booking FROM '/Users/bennylee/Documents/galvanize/Calendar/postgres/bookingData4.csv' DELIMITER ',' CSV HEADER;"))
   .then( (res) => console.log(res))
   .then( ()=>client.end)
   .catch(err => console.error('query error', err))
 
 
 
-
-// pg.connect(function(err, client, done) {
-//   var stream = client.query(copyFrom('COPY  from 'FILE_PATH.csv' WITH DELIMITER ',' CSV HEADER;'));
-//   var fileStream = fs.createReadStream('some_file.tsv')
-//   fileStream.on('error', done);
-//   fileStream.pipe(stream).on('finish', done).on('error', done);
-// });
-
-// let stream = fs.createReadStream("./place.csv");
-// let csvData = [];
-// let csvStream = fastcsv
-//   .parse()
-//   .on("data", function(data) {
-//     csvData.push(data);
-//   })
-//   .on("end", function() {
-//     // remove the first line: header
-//     csvData.shift();
-
-//     // connect to the PostgreSQL database
-//     // save csvData
-//   });
-
-// stream.pipe(csvStream);
-
-// COPY  from 'FILE_PATH.csv' WITH DELIMITER ',' CSV HEADER;
